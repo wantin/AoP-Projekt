b2d6086443f95e2ecfd8c9e31e77164d87ace928
wantin <vjmzwei@gmail.com>
2017-07-31 13:15:11 +0200

    Hauptsächlich Main: Struktur des Spielablaufs immer noch fehlerhaft wegen einheitenpaket.

--- src/einsnull/Main.java
+++ src/einsnull/Main.java
@@ -1,9 +1,48 @@
 package einsnull;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static void ziehen(Spieler links, Spieler rechts){
+		//eigentliches Spielen
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		Spieler links = new Spieler();
+		Spieler rechts = new Spieler();
+		
+		//Spieler oder KI auswählen
+		
+		//Spieler benennen
+		//TODO: switch from console to GUI
+		System.out.println("Bitte geben Sie den Namen des linken Spielers ein.");
+		links.setName(input.next());
+		System.out.println("Bitte geben Sie den Namen des rechten Spielers ein.");
+		rechts.setName(input.next());
+		
+		//Karten wählen
+		
+		
+		//Karten ausspielen oder benutzen
+		while((links.getHand() != null || links.getTruppen() != null) && 
+				(rechts.getHand() != null || rechts.getTruppen() != null)){
+			ziehen(links, rechts);
+		}
+		
+		//Sieger bekanntgeben
+		//TODO: switch from console to GUI
+		if(links.getHand() != null || links.getTruppen() != null) {
+			System.out.println(links.getName() + " hat gewonnen.");
+		}else {
+			if(rechts.getHand() != null || rechts.getTruppen() != null) {
+				System.out.println(rechts.getName() + " hat gewonnen.");
+			}else {
+				System.out.println("Die Partie endet unentschieden.");
+			}
+		}
 
 	}
 
--- src/einsnull/Spieler.java
+++ src/einsnull/Spieler.java
@@ -3,6 +3,7 @@ package einsnull;
 import einheitenkarten.Einheit;
 
 public class Spieler {
+	String name;
 	Karte[] hand;
 	Einheit[] truppen;
 	int gold;
@@ -15,10 +16,35 @@ public class Spieler {
 		this.gold = gold;
 	}
 	
-	//ich muss erstmal wieder ins Programmieren reinkommen, keine Ahnung ob die Methode hier Sinn macht
-	public void getHand() {
+	//ich muss erstmal wieder ins Programmieren reinkommen, keine Ahnung ob die Methode hier Sinn macht (Dong)
+	//umbenannt zu print. falls wir die brauchen haben wir die dann ja
+	public void printHand() {
 		for (int i = 0; i <= hand.length; i++) {
 			System.out.println(hand[i]);
 		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Karte[] getHand() {
+		return hand;
+	}
+
+	public void setHand(Karte[] hand) {
+		this.hand = hand;
+	}
+
+	public Einheit[] getTruppen() {
+		return truppen;
+	}
+
+	public void setTruppen(Einheit[] truppen) {
+		this.truppen = truppen;
 	}
 }
