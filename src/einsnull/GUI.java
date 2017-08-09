812f91b89c59275d2f8fe76c4586113efb99a3be
E.Heinz <elinaheinz@gmx.de>
2017-08-09 10:08:31 +0200

    In der GUI muss ich noch die Verbindung zwischen Karten und JButtons der beiden Spieler machen(hab dafür acht Felder vorerst gemacht, müssen noch überlegen wie das wird wenn GottesSegen ins Spiel kommt). Des Weiteren die erwünschte Anzeige auf dem mittleren Feld(ist frei oder welche Gegner mit der gerade erwählten Einheit angreifbar sind)

--- src/einsnull/GUI.java
+++ src/einsnull/GUI.java
@@ -2,10 +2,14 @@ package einsnull;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +30,17 @@ public class GUI extends JFrame{
 	private JLabel sp1k = new JLabel ("Kartenstapel");
 	private JLabel sp2k = new JLabel ("Kartenstapel");
 	Random zufall = new Random();
+	
+	
+	protected void aktion(){
+		  JButton bildbttn = new JButton();
+		  try {
+		    Image img = ImageIO.read(getClass().getResource("bilder/schneckentyp.jpg"));
+		    bildbttn.setIcon(new ImageIcon(img));
+		  } catch (Exception ex) {
+		    System.out.println(ex);
+		  }
+	}
 		
 	public GUI(){
 		
@@ -40,31 +55,63 @@ public class GUI extends JFrame{
 		
 		//Spieler 1 im linken Feld
 		links.setLayout(null);
-		links.setBackground(new Color(0,5,5));
+		links.setBackground(new Color(20,10,10));
 		links.setBounds(0,0,300,1000);
 		
 		sp1.setLocation(120,10);
 		sp1.setSize(60,40);
 		sp1.setForeground(Color.white);
 		
-		sp1z.setLocation(110,150);
+		sp1z.setLocation(123,150);
 		sp1z.setSize(80,40);
 		sp1z.setForeground(Color.white);
 		
 
-		sp1k.setLocation(110,400);
+		sp1k.setLocation(117,300);
 		sp1k.setSize(90,40);
 		sp1k.setForeground(Color.white);
+		
+		//Kartenfeld linkes Feld
+		int k,l;
+		k = 2;
+		l = 4;
+		JPanel lpanel = new JPanel();
+		lpanel.setLayout(null);
+		lpanel.setLocation(0,400);
+		lpanel.setSize(300,500);
+		lpanel.setLayout(new GridLayout(4,2,4,5));//Einteilung Panel und Zwischenabstände 
+		lpanel.setBackground(links.getBackground());
+		
+		for(int a = 0; a < k; a++){
+			for(int b = 0; b < l; b++){		
+				JButton lbttn = new JButton();
+				lbttn.setBackground(links.getBackground());
+				lbttn.addActionListener(new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent arg0){
+						aktion();
+					}
+				});
+				lpanel.add(lbttn);
+			}
+		}
+		
+
+		//Einbindung Bild zu Testzwecken
+		/*JButton bildbttn = new JButton(new ImageIcon(((new ImageIcon("bilder/söldner.jpg")).getImage()).getScaledInstance(150, 120, java.awt.Image.SCALE_SMOOTH)));	 
+	   	bildbttn.setLayout(null);
+		lpanel.add(bildbttn);*/
+		
 		
 		links.add(sp1);
 		links.add(sp1z);
 		links.add(sp1k);
-		
+		links.add(lpanel);		
 			
 		//Spielfeld
 		mitte.setLayout(null);
 		mitte.setLayout(new GridLayout(6,6));
-		mitte.setForeground(Color.orange);
+		//mitte.setForeground(Color.orange);
 		mitte.setBackground(new Color(100,200,0));
 		mitte.setBounds(300,0,900,1000);
 		
@@ -73,22 +120,25 @@ public class GUI extends JFrame{
 		m = 6;
 		n = 6;
 		JButton[][] button = new JButton[m][n];
-		for(int i = 0; i < button.length; i++){
-			for(int j = 0; j < button.length; j++){
-				button[i][j] = new JButton();
-				button[i][j].setLayout(null);
-				button[i][j].setForeground(mitte.getForeground());
-				button[i][j].setSize(mitte.getPreferredSize());
-			    button[i][j].setBackground(mitte.getBackground());
-				button[i][j].addActionListener(new ActionListener(){
+		for(int c = 0; c < m; c++){
+			for(int d = 0; d < n; d++){
+				button[c][d] = new JButton();
+				button[c][d].setLayout(null);
+				button[c][d].setForeground(mitte.getForeground());
+				button[c][d].setSize(mitte.getPreferredSize());
+			    button[c][d].setBackground(mitte.getBackground());
+				button[c][d].addActionListener(new ActionListener(){
 					
+					//muss noch abgeändert werden, da bisher der attbttn(für angreifbare einheiten) genutzt wird(auch wenn freies feld)
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
 						JButton freebttn = new JButton();
 						freebttn.setLayout(null);
 						freebttn.setSize(getPreferredSize());
 						freebttn.setForeground(mitte.getForeground());
-						System.out.println("Das Feld ist leer");
+						if(freebttn == null){
+							System.out.println("Das Feld ist leer");
+						}
 					
 						JButton attbttn = new JButton();
 						if(attbttn != null){
@@ -99,39 +149,54 @@ public class GUI extends JFrame{
 					}
 						
 				});
-				mitte.add(button[i][j]);
+				mitte.add(button[c][d]);
 			}
-		}		
+		}	
 		
 		//Spieler 2 im rechten Feld
 		rechts.setLayout(null);
 		rechts.setOpaque(true);
-		rechts.setBackground(new Color(255,255,250));
+		rechts.setBackground(new Color(245,240,200));
 		rechts.setBounds(1200,0,300,1000);
 		
 		sp2.setLocation(120,10);
 		sp2.setSize(60,40);
 		sp2.setForeground(Color.black);
 		
-		sp2z.setLocation(110,150);
+		sp2z.setLocation(123,150);
 		sp2z.setSize(80,40);
-		sp2z.setForeground(Color.black);
-		
+		sp2z.setForeground(Color.black);		
 
-		sp2k.setLocation(110,400);
+		sp2k.setLocation(117,300);
 		sp2k.setSize(90,40);
 		sp2k.setForeground(Color.black);
+		
+		//Kartenfeld rechtes Feld
+		JPanel rpanel = new JPanel();
+		rpanel.setLayout(null);
+		rpanel.setLocation(0,400);
+		rpanel.setSize(300,500);
+		rpanel.setLayout(new GridLayout(4,2,4,5));//Einteilung Panel und Zwischenabstände 
+		rpanel.setBackground(rechts.getBackground());
+		
+		for(int e = 0; e < k; e++){
+			for(int f = 0; f < l; f++){		
+				JButton rbttn = new JButton();
+				rbttn.setBackground(rechts.getBackground());
+				rpanel.add(rbttn);
+			}
+		}
 		
 		rechts.add(sp2);
 		rechts.add(sp2z);
 		rechts.add(sp2k);
+		rechts.add(rpanel);
 			
 		cp.add(mitte);
 		cp.add(links);
 		cp.add(rechts);
-		
-		
 	}
+	
 	
 
 }
